fef9c1559ab94daaf66d5e922cb238d337d91df4
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-06-11 20:37:16 -0300

    agregue el maquetado del maquetado del componente formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import FormularioTarea from "./components/FormularioTarea"
 
 function App() {
   return (
     <>
       <main className="container my-4">
         <h1 className="text-center">Lista de Tareas</h1>
+        <FormularioTarea/>
       </main>
       <footer className="bg-dark text-light text-center py-3">
         <p>&copy;Todos los derechos reservados</p>
--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,9 +1,17 @@
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
 const FormularioTarea = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <Form>
+        <Form.Group className="mb-3 d-flex gap-2">
+          <Form.Control type="text" placeholder="Ingresa una tarea" />
+          <Button variant="success">Enviar</Button>
+        </Form.Group>
+      </Form>
+    </div>
+  );
 };
 
 export default FormularioTarea;
