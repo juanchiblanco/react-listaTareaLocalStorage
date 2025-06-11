374552c5f0d7582b4cecbecc24c9393f50433d8a
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-06-11 20:49:26 -0300

    agregamos el maquetado del componente listaTareas y Itemtarea

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,16 +1,18 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import ListaTareas from "./ListaTareas";
 
 const FormularioTarea = () => {
   return (
-    <div>
+    <section>
       <Form>
         <Form.Group className="mb-3 d-flex gap-2">
           <Form.Control type="text" placeholder="Ingresa una tarea" />
           <Button variant="success">Enviar</Button>
         </Form.Group>
       </Form>
-    </div>
+      <ListaTareas></ListaTareas>
+    </section>
   );
 };
 
--- src/components/ItemTarea.jsx
+++ src/components/ItemTarea.jsx
@@ -1,9 +1,11 @@
+import { Button, ListGroup } from "react-bootstrap";
+
 const ItemTarea = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <ListGroup.Item className="d-flex justify-content-between">
+      Tarea 1 <Button variant="danger">Borrar</Button>
+    </ListGroup.Item>
+  );
 };
 
 export default ItemTarea;
