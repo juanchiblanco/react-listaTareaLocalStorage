6af8f3a38ea7a98892e6e9fdeafba5a1ae4c255d
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-08-26 20:13:09 -0300

    agregue el boton de editar

--- src/components/ItemTarea.jsx
+++ src/components/ItemTarea.jsx
@@ -2,7 +2,11 @@ import { Button, ListGroup } from "react-bootstrap";
 
 const ItemTarea = ({nombreTarea, posicion, borrarTarea}) => {
   return (
-    <ListGroup.Item className="d-flex justify-content-between">{posicion+1}. {nombreTarea}<Button type="button" variant="danger" onClick={()=> borrarTarea(nombreTarea)}>Borrar</Button>
+    <ListGroup.Item className="d-flex justify-content-between">{posicion+1}. {nombreTarea}
+    <Button variant="warning" className="ms-auto me-2 btn btn-warning text-light">
+          Editar
+        </Button>
+    <Button type="button" variant="danger" onClick={()=> borrarTarea(nombreTarea)}>Borrar</Button>
     </ListGroup.Item>
   );
 };
